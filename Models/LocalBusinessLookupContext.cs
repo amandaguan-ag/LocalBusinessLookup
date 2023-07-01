f42eb5a4b7b61871d364f4900718dc098b98c9ae
Amanda Guan <ag.amandaguan@gmail.com>
2023-07-01 13:59:38 -0700

    seed db

--- Models/LocalBusinessLookupContext.cs
+++ Models/LocalBusinessLookupContext.cs
@@ -10,5 +10,13 @@ namespace LocalBusinessLookup.Models
         }
 
         public DbSet<Business> Businesses { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Business>()
+                .HasData(
+                    new Business { BusinessId = 1, Name = "Dave's Music", Address = "123 N Street St", PhoneNumber = "7", Website = "Example.com" },
+                    new Business { BusinessId = 2, Name = "John's Sounds", Address = "456 N Way Rd", PhoneNumber = "867-5309", Website = "Example.com" }
+                );
+        }
     }
 }
