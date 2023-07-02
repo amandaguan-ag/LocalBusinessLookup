3a9f3f88c412068533ae887ae7ebc716d74bff12
Amanda Guan <ag.amandaguan@gmail.com>
2023-07-01 23:45:52 -0700

    add versioning

--- Controllers/BusinessesController.cs
+++ Controllers/BusinessesController.cs
@@ -1,20 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using LocalBusinessLookup.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace LocalBusinessLookup.Controllers
 {
-    [Route("api/[controller]")]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/businesses")]
     [ApiController]
     public class BusinessesController : ControllerBase
     {
-        private readonly LocalBusinessLookupContext _db;
+        private LocalBusinessLookupContext _db;
 
         public BusinessesController(LocalBusinessLookupContext db)
         {
             _db = db;
         }
 
+        // GET: api/Businesses
         [HttpGet]
         public ActionResult<IEnumerable<Business>> Get(string name, string address, string phoneNumber, string website)
         {
@@ -38,6 +40,120 @@ namespace LocalBusinessLookup.Controllers
             if (website != null)
             {
                 query = query.Where(entry => entry.Website == website);
+            }
+
+            return query.ToList();
+        }
+
+        // GET: api/Businesses/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Business>> GetBusiness(int id)
+        {
+            Business business = await _db.Businesses.FindAsync(id);
+
+            if (business == null)
+            {
+                return NotFound();
+            }
+
+            return business;
+        }
+        // POST api/businesses
+        [HttpPost]
+        public async Task<ActionResult<Business>> Post(Business business)
+        {
+            _db.Businesses.Add(business);
+            await _db.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetBusiness), new { id = business.BusinessId }, business);
+        }
+        // PUT: api/Businesses/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Business business)
+        {
+            if (id != business.BusinessId)
+            {
+                return BadRequest();
+            }
+
+            _db.Businesses.Update(business);
+
+            try
+            {
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BusinessExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        private bool BusinessExists(int id)
+        {
+            return _db.Businesses.Any(e => e.BusinessId == id);
+        }
+        // DELETE: api/Businesses/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBusiness(int id)
+        {
+            Business business = await _db.Businesses.FindAsync(id);
+            if (business == null)
+            {
+                return NotFound();
+            }
+
+            _db.Businesses.Remove(business);
+            await _db.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+
+    //allow more advanced search options: partial match search
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}/businesses")]
+    [ApiController]
+    public class BusinessesV2Controller : ControllerBase
+    {
+        private LocalBusinessLookupContext _db;
+
+        public BusinessesV2Controller(LocalBusinessLookupContext db)
+        {
+            _db = db;
+        }
+
+        // GET: api/Businesses
+        [HttpGet]
+        public ActionResult<IEnumerable<Business>> Get(string name, string address, string phoneNumber, string website)
+        {
+            var query = _db.Businesses.AsQueryable();
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                query = query.Where(entry => entry.Name.Contains(name));
+            }
+
+            if (!string.IsNullOrEmpty(address))
+            {
+                query = query.Where(entry => entry.Address.Contains(address));
+            }
+
+            if (!string.IsNullOrEmpty(phoneNumber))
+            {
+                query = query.Where(entry => entry.PhoneNumber.Contains(phoneNumber));
+            }
+
+            if (!string.IsNullOrEmpty(website))
+            {
+                query = query.Where(entry => entry.Website.Contains(website));
             }
 
             return query.ToList();
--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using LocalBusinessLookup.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,15 +16,42 @@ builder.Services.AddDbContext<LocalBusinessLookupContext>(
                   )
                 );
 
+builder.Services.AddVersionedApiExplorer(setup =>
+{
+    setup.GroupNameFormat = "'v'VVV";
+    setup.SubstituteApiVersionInUrl = true;
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.ConfigureOptions<ConfigureSwaggerOptions>();
+
+builder.Services.AddApiVersioning(opt =>
+                                    {
+                                        opt.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+                                        opt.AssumeDefaultVersionWhenUnspecified = true;
+                                        opt.ReportApiVersions = true;
+                                        opt.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader(),
+                                                                                        new HeaderApiVersionReader("x-api-version"),
+                                                                                        new MediaTypeApiVersionReader("x-api-version"));
+                                    });
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    var apiVersionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        foreach (var description in apiVersionDescriptionProvider.ApiVersionDescriptions)
+        {
+            options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json",
+                description.GroupName.ToUpperInvariant());
+        }
+    });
 }
 else
 {
